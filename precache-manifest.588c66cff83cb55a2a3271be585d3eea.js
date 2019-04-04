46d2e04d73cf591ac53b46971c65c18e74b3c8f8
LemesVinicius <vinilemes@live.com>
2019-04-04 12:16:52 -0300

    Updates

---
+++ precache-manifest.588c66cff83cb55a2a3271be585d3eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4b9d4610f4a0e9b2870",
+    "url": "/static/js/main.c061f5bd.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e4b9d4610f4a0e9b2870",
+    "url": "/static/css/main.75805714.chunk.css"
+  },
+  {
+    "revision": "dd47e17c83e7da94cb2ae34878637f1b",
+    "url": "/index.html"
+  }
+];
